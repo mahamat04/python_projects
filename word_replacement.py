0c469948e49b9fcca2ad9d9cd28c7442a30c91be
Mahamat04 <mahamatalhabib224@gmail.com>
2022-12-19 00:57:34 +0100

    Well done

---
+++ word_replacement.py
@@ -0,0 +1,8 @@
+def replace_word():
+    pass
+    string = input('Hello, give some words: ')
+    word_to_replace = input('Enter the word that you want to replace: ')
+    word_replacement = input('Enter the word replacement: ')
+    print(string.replace(word_to_replace, word_replacement))
+
+replace_word()
